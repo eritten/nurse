db00d10fd4f0b92e610ed2d474e3c6da77b7f690
Klasique <feboapong@gmail.com>
2023-03-08 20:50:02 +0100

    yes

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,21 +1,39 @@
 window.addEventListener("load", ()=>{
-    // menu button functionality 
+    // menu and search button functionality 
     const menuBtn = document.querySelector(".menu-btn")
+    const searchBtn = document.querySelector(".search-btn")
+    const searchBar = document.querySelector(".search-form")
     const menuList = document.querySelector(".nav-list")
     
     menuBtn.addEventListener("click", toggleNav)
+    searchBtn.addEventListener("click", toggleSearch)
 
     function toggleNav() {
         menuList.classList.toggle("active")
+        removeSearch()
         if (menuList.classList.contains("active")){
             menuBtn.innerHTML = `<i class="fas fa-times"></i>`
         }else {
             menuBtn.innerHTML = `<i class="fas fa-bars"></i>`
         }
     }
-
+    function toggleSearch() {
+        searchBar.classList.toggle("active")
+        removeNav()
+        menuBtn.innerHTML = `<i class="fas fa-bars"></i>`
+    }
+    function removeSearch() {
+        searchBar.classList.remove("active")
+    }
+    function removeNav() {
+        menuList.classList.remove("active")
+    }
+    // disappear on scroll
     window.addEventListener("scroll", ()=>{
         menuList.classList.remove("active", screenY > 0)
         menuBtn.innerHTML = `<i class="fas fa-bars"></i>`
+        removeSearch()
     })
+
+
 })
